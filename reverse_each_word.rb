35d44c8f17785527f9826831341819cb6f8f19c3
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 06:25:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   array = sentence1.split(" ")
-  array.collect {|word| word.reverse}
+  array.collect {|word| word.reverse!}
   array.join(" ")
   return array
 end
