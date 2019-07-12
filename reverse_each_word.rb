14668ac8b64a2b46b4d3ea20ad711b75d64d47c0
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 06:12:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,4 @@
 def reverse_each_word(string)
-  word_array = string.split(" ").collect {|word| word.reverse}.join
+  word_array = string.split(" ").collect {|word| word.reverse}.join(" ")
+  return word_array
+end
